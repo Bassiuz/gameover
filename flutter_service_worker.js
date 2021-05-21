1e5389fadad57d595e1c357ecb627a3f47cce154
Bas de Vaan <bas.de.vaan@kabisa.nl>
2021-05-21 16:20:10 +0200

    push updated build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6d501bcb63ceb13b648ef81fce109c46",
-"index.html": "ee53691f2c50bd2b30d69b3ee500371e",
-"/": "ee53691f2c50bd2b30d69b3ee500371e",
-"main.dart.js": "ba0ee868e726ddf95450c067057c5882",
+"index.html": "9a98b6eb3c8f629e0140ab6406c5c075",
+"/": "9a98b6eb3c8f629e0140ab6406c5c075",
+"main.dart.js": "2409e7840858ef99089f04e7a5bdf3fe",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "b53b45d6a5c653fcf50a6665ac9390ba",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "6bb7fa08eaad6bfa999420a186588c07",
+"assets/NOTICES": "e51ba5ae85281716b34b7818703ed143",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
